4b4bf1f8adb439d7415fcc913157e418b2b54238
Martin Luna <maluna@technisys.com>
2022-01-10 16:21:31 -0300

    Added go back Link using react router

--- src/components/DogDetails.jsx
+++ src/components/DogDetails.jsx
@@ -1,10 +1,8 @@
-import hazel from "../images/hazel.jpg";
-import tubby from "../images/tubby.jpg";
-import whiskey from "../images/whiskey.jpg";
-
+import { Link } from "react-router-dom";
 export const DogDetails = ({ dog }) => {
   return (
     <div>
+      <Link to="/dogs">Go back</Link>
       <h1>{dog.name}</h1>
       <img src={`../src/images/${dog.src}.jpg`} alt={dog.name} />
       <p>Age: {dog.age}</p>
