a2001e950c342cfd69c99559aa97fd0ac9a9baa2
Martin Luna <maluna@technisys.com>
2022-01-10 19:35:56 -0300

    Updated markup with classnames

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,18 +1,34 @@
 import { NavLink } from "react-router-dom";
-import "./NavBar.css";
 
 export const NavBar = ({ dogs }) => {
   return (
-    <nav className="NavBar">
-      <NavLink exact to="/dogs">
-        Dogs
-      </NavLink>
-      <div>
-        {dogs.map((dog) => (
-          <NavLink exact key={dog.name} to={`/dogs/${dog.name}`}>
-            {dog.name}
-          </NavLink>
-        ))}
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <div class="container-fluid">
+        <NavLink exact to="/dogs" className="navbar-brand">
+          Dogs
+        </NavLink>
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav">
+            {dogs.map((dog) => (
+              <li key={dog.name} className="nav-item">
+                <NavLink exact to={`/dogs/${dog.name}`} className="nav-link">
+                  {dog.name}
+                </NavLink>
+              </li>
+            ))}
+          </ul>
+        </div>
       </div>
     </nav>
   );
