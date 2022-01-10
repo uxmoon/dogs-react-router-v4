597115e322c49dc669cbfd323fe6c7572597c8f3
Martin Luna <maluna@technisys.com>
2022-01-10 15:26:14 -0300

    Impor dogs data JSON and new List component

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
 import { Switch, Route } from "react-router-dom";
+import { DogsList } from "./components/DogsList";
+import dogs from "./dogs.json";
+
 import "./App.css";
 
 function App() {
-  return <Route path="/dogs" render={() => <h1>Dogs list</h1>} />;
+  return <Route path="/dogs" render={() => <DogsList dogs={dogs} />} />;
 }
 
 export default App;
