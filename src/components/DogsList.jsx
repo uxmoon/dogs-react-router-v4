4b4c4efb86315125afc0c137e09def1eca66f7cb
Martin Luna <maluna@technisys.com>
2022-01-10 20:54:17 -0300

    Added styles to dogs list

--- src/components/DogsList.jsx
+++ src/components/DogsList.jsx
@@ -1,15 +1,20 @@
 import { Link } from "react-router-dom";
+import "./DogsList.css";
 
 export const DogsList = ({ dogs }) => {
   return (
-    <div className="container">
+    <div className="DogsList container marketing">
       <div className="row">
         {dogs.map((dog) => (
-          <div className="col-12 col-sm-6 col-md-4" key={dog.name}>
+          <div
+            className="DogsList-dog col-12 col-sm-6 col-md-4 text-center"
+            key={dog.name}
+          >
             <img src={`src/images/${dog.src}.jpg`} alt={dog.name} />
-            <h2 className="text-center">
-              <Link to={`/dogs/${dog.name}`}>{dog.name}</Link>
-            </h2>
+            <h2 className="text-center mb-2">{dog.name}</h2>
+            <Link to={`/dogs/${dog.name}`} className="btn btn-secondary">
+              View details
+            </Link>
           </div>
         ))}
       </div>
