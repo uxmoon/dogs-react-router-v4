2808f771febe3d4da1f03c850279570e6411d6ee
Martin Luna <maluna@technisys.com>
2022-01-10 20:35:52 -0300

    Added bootstrap styles

--- src/components/DogDetails.jsx
+++ src/components/DogDetails.jsx
@@ -1,16 +1,29 @@
 import { Link } from "react-router-dom";
 export const DogDetails = ({ dog }) => {
   return (
-    <div>
-      <Link to="/dogs">Go back</Link>
-      <h1>{dog.name}</h1>
-      <img src={`../src/images/${dog.src}.jpg`} alt={dog.name} />
-      <p>Age: {dog.age}</p>
-      <ul>
-        {dog.facts.map((item) => (
-          <li key={item}>{item}</li>
-        ))}
-      </ul>
-    </div>
+    <>
+      <Link to="/dogs" className="btn btn-secondary mb-4">
+        Go back
+      </Link>
+
+      <div className="card" style={{ width: "18rem" }}>
+        <img
+          src={`../src/images/${dog.src}.jpg`}
+          alt={dog.name}
+          className="card-img-top"
+        />
+        <div className="card-body">
+          <h1 class="card-title h5">{dog.name}</h1>
+          <h2 class="card-subtitle mb-2 text-muted h6">Age: {dog.age}</h2>
+        </div>
+        <ul className="list-group list-group-flush">
+          {dog.facts.map((item) => (
+            <li key={item} className="list-group-item">
+              {item}
+            </li>
+          ))}
+        </ul>
+      </div>
+    </>
   );
 };
--- src/components/DogsList.jsx
+++ src/components/DogsList.jsx
@@ -1,17 +1,18 @@
 import { Link } from "react-router-dom";
-import "./DogsList.css";
 
 export const DogsList = ({ dogs }) => {
   return (
-    <ul className="DogsList">
-      {dogs.map((dog) => (
-        <li key={dog.name}>
-          <img src={`src/images/${dog.src}.jpg`} alt={dog.name} />
-          <h2>
-            <Link to={`/dogs/${dog.name}`}>{dog.name}</Link>
-          </h2>
-        </li>
-      ))}
-    </ul>
+    <div className="container">
+      <div className="row">
+        {dogs.map((dog) => (
+          <div className="col-12 col-sm-6 col-md-4" key={dog.name}>
+            <img src={`src/images/${dog.src}.jpg`} alt={dog.name} />
+            <h2 className="text-center">
+              <Link to={`/dogs/${dog.name}`}>{dog.name}</Link>
+            </h2>
+          </div>
+        ))}
+      </div>
+    </div>
   );
 };
