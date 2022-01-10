873f980c72aa101095bcdf082f4fd9d96895aafe
Martin Luna <maluna@technisys.com>
2022-01-10 15:57:22 -0300

    Render details page

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,27 @@
 import { Switch, Route } from "react-router-dom";
 import { DogsList } from "./components/DogsList";
+import { DogDetails } from "./components/DogDetails";
 import dogs from "./dogs.json";
 
 import "./App.css";
 
 function App() {
-  return <Route path="/dogs" render={() => <DogsList dogs={dogs} />} />;
+  const getDog = (props) => {
+    // use RouterProps to get name
+    let name = props.match.params.name;
+
+    // use find() on dogs json to find a name match
+    let current = dogs.find(
+      (dog) => dog.name.toLowerCase() === name.toLowerCase()
+    );
+    return <DogDetails {...props} dog={current} />;
+  };
+  return (
+    <Switch>
+      <Route exact path="/dogs" render={() => <DogsList dogs={dogs} />} />
+      <Route exact path="/dogs/:name" render={getDog} />
+    </Switch>
+  );
 }
 
 export default App;
