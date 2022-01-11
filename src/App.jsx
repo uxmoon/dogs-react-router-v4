05110a2ba9023f0e54ac87bd77014aee39176ff1
Martin Luna <maluna@technisys.com>
2022-01-11 13:08:12 -0300

    Added react helmet

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import { NavBar } from "./components/NavBar";
 import { Routes } from "./components/Routes";
+import { Helmet } from "react-helmet";
 import dogs from "./dogs.json";
 import "./App.css";
 
 function App() {
   return (
     <>
+      <Helmet>
+        <title>Dog App</title>
+      </Helmet>
       <NavBar dogs={dogs} />
       <Routes dogs={dogs} />
     </>
