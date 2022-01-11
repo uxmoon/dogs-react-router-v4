8fcec6a054720348254cf6463730d3566e184de6
Martin Luna <maluna@technisys.com>
2022-01-11 11:54:48 -0300

    Added google fonts for names and updated images

--- src/components/DogsList.jsx
+++ src/components/DogsList.jsx
@@ -3,16 +3,15 @@ import "./DogsList.css";
 
 export const DogsList = ({ dogs }) => {
   return (
-    <div className="DogsList container marketing">
+    <div className="container pt-4">
       <div className="row">
         {dogs.map((dog) => (
-          <div
-            className="DogsList-dog col-12 col-sm-6 col-md-4 text-center"
-            key={dog.name}
-          >
-            <img src={`src/images/${dog.src}.jpg`} alt={dog.name} />
-            <h2 className="text-center mb-2">{dog.name}</h2>
-            <Link to={`/dogs/${dog.name}`} className="btn btn-secondary">
+          <div className="col-12 col-sm-6 col-md-4 text-center" key={dog.name}>
+            <div className="DogsList-thumbnail">
+              <img src={`src/images/${dog.src}.jpg`} alt={dog.name} />
+            </div>
+            <h2 className="DogsList-name text-center mb-4">{dog.name}</h2>
+            <Link to={`/dogs/${dog.name}`} className="btn btn-primary">
               View details
             </Link>
           </div>
