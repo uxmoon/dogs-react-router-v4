d534497885d911f8fc975c241a013d5cf2e0eb75
Martin Luna <maluna@technisys.com>
2022-01-10 22:13:52 -0300

    Added sass and updated vite js config

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,24 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+const path = require("path");
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     port: 3001,
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "/src"),
+      "~bootstrap": "bootstrap",
+    },
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@import "./src/scss/variables";`,
+      },
+    },
   },
 });
